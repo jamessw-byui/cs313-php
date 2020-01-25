8f5f307c3edc0e92b83594b90d51d00419fd3912
Jim White <jamessw@byui.edu>
2020-01-25 11:02:06 -0700

    updating images

--- web/week03/browse.php
+++ web/week03/browse.php
@@ -23,27 +23,27 @@ session_start();
     </div> <!-- end of header -->
     <div class="main">
         <div class="song">
-            <img src="12Tribes.png" alt="12TribesSong" width="100" height="100">
+            <img src="12Tribes.png" alt="12TribesSong">
             <button type="button" id="12Tribes">Add to Cart</button>
         </div> 
         <div class="song">
-            <img src="AskSeek.png" alt="AskSeekSong" width="100" height="100">
+            <img src="AskSeek.png" alt="AskSeekSong">
             <button type="button" id="AskSeek">Add to Cart</button>
         </div> 
         <div class="song">
-            <img src="BoM.png" alt="BookOfMormonSong" width="100" height="100">
+            <img src="BoM.png" alt="BookOfMormonSong">
             <button type="button" id="BoM">Add to Cart</button>
         </div> 
         <div class="song">
-            <img src="LackWisdom.png" alt="LackWisdomSong" width="100" height="100">
+            <img src="LackWisdom.png" alt="LackWisdomSong">
             <button type="button" id="LackWisdom">Add to Cart</button>
         </div> 
         <div class="song">
-            <img src="LivingChrist.png" alt="LivingChristSong" width="100" height="100">
+            <img src="LivingChrist.png" alt="LivingChristSong">
             <button type="button" id="LivingChrist">Add to Cart</button>
         </div> 
         <div class="song">
-            <img src="Trust.png" alt="TrustSong" width="100" height="100">
+            <img src="Trust.png" alt="TrustSong">
             <button type="button" id="Trust">Add to Cart</button>
         </div>
     </div> <!-- end of main -->
