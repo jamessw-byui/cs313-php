e5a93d733e4855abce89d899434eeb7540c307f0
Jim White <jamessw@byui.edu>
2020-01-25 21:51:43 -0700

    adding more logs

--- web/week03/week03.js
+++ web/week03/week03.js
@@ -1,12 +1,13 @@
 function addToCart(song) {
-	$.ajax({
+    $.ajax({
         type: "POST",
         url: "addSong.php" ,
         data: { "s": song },
         success : function() {
-        	console.log('Received Success');
+        	console.log(result);
+            console.log('Success Received');
             // function below reloads current page
-            location.reload();
+            // location.reload();
 
         }
     });
